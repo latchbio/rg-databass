9bf5808ebc981c68586415f63fd0ced4bd07c92e
AidanAbd <aidan@latch.bio>
2023-08-01 09:37:42 -0700

    fix: can parse the simplest of messages lol

--- code/aidanabd/pb3_simple_parser/src/main.rs
+++ code/aidanabd/pb3_simple_parser/src/main.rs
@@ -43,11 +43,6 @@ fn serialize_test_data(data: &test_data::Datum) -> Vec<u8> {
     buf
 }
 
-const VARINT_PAYLOAD_LENGTH: u8 = 7;
-const VARINT_MSB_MASK: u8 = 1 << VARINT_PAYLOAD_LENGTH;
-const MAX_VARINT_PAYLOAD: u8 = VARINT_MSB_MASK - 1;
-const VARINT_PAYLOAD_MASK: u8 = !VARINT_MSB_MASK & 0xFF;
-
 fn try_read_byte(buf: &[u8], position: usize) -> (u8, Option<usize>) {
     if position >= buf.len() {
         return (0, None);
@@ -56,35 +51,50 @@ fn try_read_byte(buf: &[u8], position: usize) -> (u8, Option<usize>) {
     (buf[position], Some(position + 1))
 }
 
-fn decode_varint(buf: &[u8], position: usize) -> (u64, usize) {
-    let mut res: u64 = 0;
+const VARINT_PAYLOAD_LEN: usize = 7;
+const VARINT_MSB_MASK: u8 = 1 << VARINT_PAYLOAD_LEN;
+const MAX_VARINT_PAYLOAD: u8 = VARINT_MSB_MASK - 1;
+const VARINT_PAYLOAD_MASK: u8 = !VARINT_MSB_MASK & 0xFF;
+
+fn parse_varint(buf: &[u8], mut position: usize) -> (u64, usize) {
+    let mut res = 0;
     let mut shift = 0;
 
     let mut msb = VARINT_MSB_MASK;
-    let mut next_position = position;
-    while msb == VARINT_MSB_MASK {
-        let (byte, new_position) = try_read_byte(buf, next_position);
 
-        if new_position.is_none() {
+    loop {
+        let try_current_byte = try_read_byte(buf, position);
+        if try_current_byte.1.is_none() {
             panic!("Unexpected end of buffer");
         }
-        next_position = new_position.unwrap();
 
-        msb = byte & VARINT_MSB_MASK;
-        let payload = byte & VARINT_PAYLOAD_MASK;
+        let current_byte = try_current_byte.0;
+        position = try_current_byte.1.unwrap();
 
-        res += u64::from(payload << shift);
-        shift += VARINT_PAYLOAD_LENGTH;
+        msb = current_byte & VARINT_MSB_MASK;
+        let payload = current_byte & VARINT_PAYLOAD_MASK;
+
+        res += u64::from(payload) << shift;
+        shift += VARINT_PAYLOAD_LEN;
+
+        if res > u64::from(MAX_VARINT_PAYLOAD) << shift {
+            panic!("Varint overflow");
+        }
+
+        if msb != VARINT_MSB_MASK {
+            break;
+        }
     }
 
-    (res, next_position)
+    (res, position)
 }
 
-fn field_tag_parse(buf: &[u8], position: usize) -> (FieldType, usize) {
-    let (varint, new_position) = decode_varint(buf, position);
+fn parse_wire_type_and_field_number(buf: &[u8], position: usize) -> (FieldType, u64, usize) {
+    let (varint, new_position) = parse_varint(buf, position);
     let field_number = varint >> 3;
+    let wire_type = varint & 0x07;
 
-    let enum_value = match field_number {
+    let parsed_wire_type = match wire_type {
         0 => FieldType::VarInt,
         1 => FieldType::I64,
         2 => FieldType::Len,
@@ -94,36 +104,35 @@ fn field_tag_parse(buf: &[u8], position: usize) -> (FieldType, usize) {
         _ => panic!("Unknown field type"),
     };
 
-    (enum_value, new_position)
+    (parsed_wire_type, field_number, new_position)
 }
 
 fn parse_test_data(buf: Vec<u8>) -> test_data::Datum {
     let buf = buf.as_slice();
     let mut position = 0;
+
+    let mut res = test_data::Datum::default();
     while buf.len() > position {
-        let (field_type, new_position) = field_tag_parse(buf, position);
+        let (field_type, field_number, new_position) =
+            parse_wire_type_and_field_number(buf, position);
         position = new_position;
 
-        println!("field_type: {:?}, position: {:?}", field_type, position);
+        println!(
+            "field_type: {:?}, field_number: {:?}, position: {:?}",
+            field_type, field_number, position
+        );
 
         match field_type {
             FieldType::VarInt => {
-                let (value, new_position) = decode_varint(buf, position);
+                let (value, new_position) = parse_varint(buf, position);
                 position = new_position;
-                println!("value: {:?}", value);
-            }
-            FieldType::I64 => {
-                let (value, new_position) = decode_varint(buf, position);
-                position = new_position;
-                println!("value: {:?}", value);
-            }
-            FieldType::I32 => {
-                let (value, new_position) = decode_varint(buf, position);
-                position = new_position;
-                println!("value: {:?}", value);
+
+                res.kind = Some(test_data::datum::Kind::NumberValue(
+                    value.try_into().unwrap(),
+                ));
             }
             FieldType::Len => {
-                let (len, new_position) = decode_varint(buf, position);
+                let (len, new_position) = parse_varint(buf, position);
                 println!("len: {:?}", len);
                 position = new_position;
                 let raw_string = &buf[position..position + len as usize];
@@ -132,20 +141,31 @@ fn parse_test_data(buf: Vec<u8>) -> test_data::Datum {
                     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
                 };
                 position += len as usize;
-                println!("value: {:?}", value);
+
+                res.kind = Some(test_data::datum::Kind::StringValue(value.to_string()));
             }
             _ => panic!("Not implemented"),
         }
     }
 
-    test_data::Datum::default()
+    return res;
 }
 
 fn main() -> io::Result<()> {
-    let test_data = get_test_data_one();
-    let serial_test_data = serialize_test_data(&test_data);
-    println!("test_data: {:?}", test_data);
-    println!("serial_test_data: {:?}", serial_test_data);
-    let parsed_test_data = parse_test_data(serial_test_data);
+    let test_data_one = get_test_data_one();
+    println!("test_data_one: {:?}", test_data_one);
+    let serial_test_data_one = serialize_test_data(&test_data_one);
+    println!("serial_test_data_one: {:?}", serial_test_data_one);
+    let deserial_test_data_one = parse_test_data(serial_test_data_one);
+    println!("deserial_test_data_one: {:?}", deserial_test_data_one);
+
+    println!();
+
+    let test_data_two = get_test_data_two();
+    println!("test_data_two: {:?}", test_data_two);
+    let serial_test_data_two = serialize_test_data(&test_data_two);
+    println!("serial_test_data_two: {:?}", serial_test_data_two);
+    let deserial_test_data_two = parse_test_data(serial_test_data_two);
+    println!("deserial_test_data_two: {:?}", deserial_test_data_two);
     Ok(())
 }
